ad3009fba5fd37c4dc63260fec24634d42e5c540
avannaxay <123742961+avannaxay@users.noreply.github.com>
2023-09-19 08:31:22 +0200

    Add files via upload

--- Projet vélo.R
+++ Projet vélo.R
@@ -6,5 +6,19 @@ library(jsonlite)
 base <- 'https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=80c6781a62951302976c8ecc3d565a095f877446'
 raw_data <- GET(base)
 
-tableauvélo <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
+tableauvelo <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
+
+
+install.packages("RMySQL")
+library(RMySQL)
+
+con <- dbConnect(MySQL(),
+                    user = "sql11646683",
+                    password = "97qYcP8UNj",
+                    host = "sql11.freesqldatabase.com",
+                    dbname = "sql11646683")
+#create table
+dbWriteTable(con, "Communes", tableauvelo)
+dbGetQuery(con, "SELECT * FROM Communes LIMIT 5;")
+
 
